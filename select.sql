8216454295231c96c6a7ce4dfa2611b7f86b298d
NikolaiZarudnev <47946322+NikolaiZarudnev@users.noreply.github.com>
2024-11-11 13:21:35 +0700

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,4 +1,4 @@
-SELECT id as position_id, prices ->> 'Name' as good_name FROM positions p
-JOIN json_array_elements(fields -> 'Goods') as prices on (prices ->> 'Price')::int = 100
+SELECT id as position_id, goods->>'Name' as good_name FROM positions p
+JOIN json_array_elements(fields->'Goods') as goods on (goods->>'Price')::int = 100
 WHERE id IN (SELECT MAX(id) FROM positions GROUP BY fields->>'Buyer')
 ;
